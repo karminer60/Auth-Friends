0c6919c9d2ba7616e122a52689fcbf5b04e758ed
karina-rodriguez <karminer60@gmail.com>
2020-06-17 09:28:35 -0700

    added post request for credentials

--- src/login.js
+++ src/login.js
@@ -20,6 +20,9 @@ class Login extends React.Component {
 
   login = e => {
     e.preventDefault();
+    axios.post('http://localhost:5000/api/login', this.state.credentials)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   };
 
   render() {
